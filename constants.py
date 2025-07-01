33bafa8ba8ac4ae83f4b03932ef3d701fad8e936
ssk <developer.kss@gmail.com>
2025-07-01 19:41:32 +0900

    pygame base code

---
+++ constants.py
@@ -0,0 +1,2 @@
+FPS = 60
+COLOR_BLACK = (0, 0, 0)
--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-print("Hello minesweeper")
+import sys
+
+import pygame
+
+from constants import *
+
+pygame.init()
+
+surface = pygame.display.set_mode((320, 240))
+
+pygame.display.set_caption("Minesweeper pygame")
+
+fps = pygame.time.Clock()
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        surface.fill(COLOR_BLACK)
+
+        pygame.display.flip()
+        
+        fps.tick(FPS)
+
+
+if __name__ == "__main__":
+    main()

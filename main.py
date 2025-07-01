4fc79a17ba01af49d3af745368e16ba08e4812c7
ssk <developer.kss@gmail.com>
2025-07-01 20:26:43 +0900

    draw line and mines

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import random
 
 import pygame
 
@@ -13,7 +14,24 @@ pygame.display.set_caption("Minesweeper pygame")
 fps = pygame.time.Clock()
 
 
+def place_mines(cells, width, height, count):
+    placed = 0
+
+    while placed < count:
+        x = random.randint(0, width - 1)
+        y = random.randint(0, height - 1)
+
+        if cells[y][x] == 0:
+            cells[y][x] = 1
+            placed += 1
+
+
 def main():
+    width = int(SCREEN_WIDTH / LINE_SIZE)
+    height = int(SCREEN_HEIGHT / LINE_SIZE)
+    cells = [[0 for x in range(int(width))] for y in range(int(height))]
+    place_mines(cells, width, height, count=30)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -24,9 +42,13 @@ def main():
 
         surface.fill(COLOR_BLACK)
 
-        for y in range(int(SCREEN_HEIGHT / LINE_SIZE)):
-            for x in range(int(SCREEN_WIDTH / LINE_SIZE)):
-                pygame.draw.rect(surface, COLOR_LINE, (x * LINE_SIZE, y * LINE_SIZE, LINE_SIZE, LINE_SIZE), 1)
+        for y in range(int(height)):
+            for x in range(int(width)):
+                rect = (x * LINE_SIZE, y * LINE_SIZE, LINE_SIZE, LINE_SIZE)
+                pygame.draw.rect(surface, COLOR_LINE, rect, 1)
+                if cells[y][x] == 1:
+                    rect = (x * LINE_SIZE + 1, y * LINE_SIZE + 1, LINE_SIZE - 2, LINE_SIZE - 2)
+                    pygame.draw.rect(surface, COLOR_MINE, rect, 0)
 
         pygame.display.flip()
 

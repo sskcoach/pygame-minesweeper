4d86aca14cbfa524c4f0498b9792a4d172020d34
ssk <developer.kss@gmail.com>
2025-07-02 19:04:57 +0900

    click to open

--- board.py
+++ board.py
@@ -4,12 +4,16 @@ import pygame
 
 from constants import *
 
+STATE_OPEN = "O"
+BOARD_MINE = "M"
+
 
 class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.cells = [[None for x in range(width)] for y in range(height)]
+        self.state = [[None for x in range(width)] for y in range(height)]
         self.number_font = pygame.font.Font(None, 15)
 
     def is_mine(self, x, y):
@@ -27,6 +31,14 @@ class Board:
             if not self.is_mine(x, y):
                 self.set_cell(x, y, BOARD_MINE)
                 placed += 1
+
+    def set_state(self, pos, value):
+        (x, y) = pos
+        print(f"set_state {pos} {self.get_state(pos)} -> {value}")
+        self.state[y][x] = value
+
+    def get_state(self, pos):
+        return self.state[pos[1]][pos[0]]
 
     def set_cell(self, x, y, value):
         self.cells[y][x] = value
@@ -56,20 +68,30 @@ class Board:
     def draw(self, surface):
         for y in range(self.height):
             for x in range(self.width):
-                rect = (x * LINE_SIZE, y * LINE_SIZE, LINE_SIZE, LINE_SIZE)
-                pygame.draw.rect(surface, COLOR_LINE, rect, 1)
-                item = self.get_cell(x, y)
-                if item == BOARD_MINE:
-                    rect = (x * LINE_SIZE + 1, y * LINE_SIZE + 1, LINE_SIZE - 2, LINE_SIZE - 2)
-                    pygame.draw.rect(surface, COLOR_MINE, rect, 0)
-                else:
-                    text = self.number_font.render(f"{item}", True, (255, 255, 0), (0, 0, 0))
-                    text_rect = text.get_rect(
-                        center=(x * LINE_SIZE + LINE_SIZE / 2, y * LINE_SIZE + LINE_SIZE / 2))
-                    surface.blit(text, text_rect)
+                self.draw_cell(surface, x, y)
+
+    def draw_cell(self, surface, x, y):
+        rect = (x * LINE_SIZE, y * LINE_SIZE, LINE_SIZE, LINE_SIZE)
+        inner_rect = (x * LINE_SIZE + 1, y * LINE_SIZE + 1, LINE_SIZE - 2, LINE_SIZE - 2)
+
+        pygame.draw.rect(surface, COLOR_LINE, rect, 1)
+        item = self.get_cell(x, y)
+        state = self.get_state((x, y))
+
+        if state is None:
+            pygame.draw.rect(surface, COLOR_INITIAL, inner_rect, 0)
+        elif state == STATE_OPEN:
+            pygame.draw.rect(surface, COLOR_OPEN, inner_rect, 0)
+
+        if item != BOARD_MINE:
+            text = self.number_font.render(f"{item}", True, (255, 255, 0), (0, 0, 0))
+            text_rect = text.get_rect(
+                center=(x * LINE_SIZE + LINE_SIZE / 2, y * LINE_SIZE + LINE_SIZE / 2))
+            surface.blit(text, text_rect)
 
     def open(self, pos):
         print(f"open: {pos[0]} {pos[1]}")
+        self.set_state(pos, STATE_OPEN)
 
     def open_around(self, pos):
         print(f"open_around: {pos[0]} {pos[1]}")

a00dc5def3181286d6c8bd846a80113b191a5ca0
ssk <developer.kss@gmail.com>
2025-07-02 19:19:46 +0900

    (x, y) = pos

--- board.py
+++ board.py
@@ -16,7 +16,8 @@ class Board:
         self.state = [[None for x in range(width)] for y in range(height)]
         self.number_font = pygame.font.Font(None, 15)
 
-    def is_mine(self, x, y):
+    def is_mine(self, pos):
+        (x, y) = pos
         if x < 0 or x >= self.width or y < 0 or y >= self.height:
             return False
         return self.cells[y][x] == BOARD_MINE
@@ -28,8 +29,8 @@ class Board:
             x = random.randint(0, self.width - 1)
             y = random.randint(0, self.height - 1)
 
-            if not self.is_mine(x, y):
-                self.set_cell(x, y, BOARD_MINE)
+            if not self.is_mine((x, y)):
+                self.set_cell((x, y), BOARD_MINE)
                 placed += 1
 
     def set_state(self, pos, value):
@@ -40,20 +41,23 @@ class Board:
     def get_state(self, pos):
         return self.state[pos[1]][pos[0]]
 
-    def set_cell(self, x, y, value):
+    def set_cell(self, pos, value):
+        (x, y) = pos
         self.cells[y][x] = value
 
-    def get_cell(self, x, y):
+    def get_cell(self, pos):
+        (x, y) = pos
         return self.cells[y][x]
 
-    def get_mine_number_around(self, x, y):
-        if self.get_cell(x, y) == BOARD_MINE:
+    def get_mine_number_around(self, pos):
+        (x, y) = pos
+        if self.get_cell(pos) == BOARD_MINE:
             return None
 
         result = 0
         for y_index in [-1, 0, 1]:
             for x_index in [-1, 0, 1]:
-                if self.is_mine(x + x_index, y + y_index):
+                if self.is_mine((x + x_index, y + y_index)):
                     result += 1
 
         return result
@@ -61,21 +65,22 @@ class Board:
     def calculate_numbers(self):
         for y in range(self.height):
             for x in range(self.width):
-                number = self.get_mine_number_around(x, y)
+                number = self.get_mine_number_around((x, y))
                 if number is not None:
-                    self.set_cell(x, y, number)
+                    self.set_cell((x, y), number)
 
     def draw(self, surface):
         for y in range(self.height):
             for x in range(self.width):
-                self.draw_cell(surface, x, y)
+                self.draw_cell(surface, (x, y))
 
-    def draw_cell(self, surface, x, y):
+    def draw_cell(self, surface, pos):
+        (x, y) = pos
         rect = (x * LINE_SIZE, y * LINE_SIZE, LINE_SIZE, LINE_SIZE)
         inner_rect = (x * LINE_SIZE + 1, y * LINE_SIZE + 1, LINE_SIZE - 2, LINE_SIZE - 2)
 
         pygame.draw.rect(surface, COLOR_LINE, rect, 1)
-        item = self.get_cell(x, y)
+        item = self.get_cell((x, y))
         state = self.get_state((x, y))
 
         if state is None:

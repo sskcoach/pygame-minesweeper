34103bbd968ac25141b227c9c1737c6fd07aae3c
ssk <developer.kss@gmail.com>
2025-07-16 22:08:28 +0900

    refactoring - extract main routine

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import pygame
 import sys
 
-pygame.init()
+def main():
+    pygame.init()
 
-surface = pygame.display.set_mode((320, 240))
+    surface = pygame.display.set_mode((320, 240))
 
-fps = pygame.time.Clock()
+    fps = pygame.time.Clock()
 
-running = True
+    running = True
 
-while running:
+    while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    surface.fill((0, 0, 0))
+        surface.fill((0, 0, 0))
 
-    size = 26
-    start_x = 320 / 2 - (size * 9) / 2
-    start_y = 240 / 2 - (size * 9) / 2
-    COLOR_WHITE = (255, 255, 255)
+        size = 26
+        start_x = 320 / 2 - (size * 9) / 2
+        start_y = 240 / 2 - (size * 9) / 2
+        COLOR_WHITE = (255, 255, 255)
 
-    for y in range(9):
-        for x in range(9):
-            rect = (start_x + x * size, start_y + y * size, size + 1, size + 1)
-            pygame.draw.rect(surface, COLOR_WHITE, rect, 1)
+        for y in range(9):
+            for x in range(9):
+                rect = (start_x + x * size, start_y + y * size, size + 1, size + 1)
+                pygame.draw.rect(surface, COLOR_WHITE, rect, 1)
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-    fps.tick(60)
+        fps.tick(60)
 
-pygame.quit()
-sys.exit()
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()

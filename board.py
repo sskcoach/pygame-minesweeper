e89295e7430c9a8c2d7b7ae82569a36e90ad0c5c
ssk <developer.kss@gmail.com>
2025-07-24 23:09:49 +0900

    open, mark

--- board.py
+++ board.py
@@ -59,19 +59,30 @@ class Board:
 
                 fill_rect = (start_x + x * size + 2, start_y + y * size + 2, size + 1 - 3, size + 1 - 3)
                 state = self.state_field[y][x]
+                pygame.draw.rect(surface, GREY, fill_rect, 0)
+
                 if state == STATE_HIDDEN:
-                    pygame.draw.rect(surface, GREY, fill_rect, 0)
+                    pass
                 elif state == STATE_OPEN:
+                    pygame.draw.rect(surface, BLACK, fill_rect, 0)
                     mine = self.mine_field[y][x]
-                    text = self.font.render(f"{mine}", True, WHITE, None)
-                    text_rect = text.get_rect(center = (start_x + x * size + size / 2, start_y + y * size + size / 2))
-                    surface.blit(text, text_rect)
+                    self.draw_text(surface, (x, y), mine, WHITE)
+                else:
+                    self.draw_text(surface, (x, y), state, BLACK)
 
 
+    def draw_text(self, surface, pos, title, color):
+        x, y = pos
+        size = int(SCREEN_HEIGHT / 9)
+        start_x = SCREEN_WIDTH / 2 - (size * 9) / 2
+        start_y = SCREEN_HEIGHT / 2 - (size * 9) / 2
 
+        text = self.font.render(f"{title}", True, color, None)
+        text_rect = text.get_rect(
+            center=(start_x + x * size + size / 2, start_y + y * size + size / 2))
+        surface.blit(text, text_rect)
 
     def on_click(self, pos, button):
-        print(f"pos: {pos} button: {button}")
         size = int(SCREEN_HEIGHT / 9)
         start_x = SCREEN_WIDTH / 2 - (size * 9) / 2
         start_y = SCREEN_HEIGHT / 2 - (size * 9) / 2
@@ -84,6 +95,26 @@ class Board:
         if size * 9 < relative_pos[1]: return
 
         index_pos = (int(relative_pos[0] / size), int(relative_pos[1] / size))
-        print(f"index_pos {index_pos}")
 
-        self.state_field[index_pos[1]][index_pos[0]] = STATE_OPEN
+        if button == pygame.BUTTON_LEFT:
+            self.open(index_pos)
+        elif button == pygame.BUTTON_RIGHT:
+            self.mark(index_pos)
+
+
+    def open(self, index_pos):
+        state = self.state_field[index_pos[1]][index_pos[0]]
+        if state == STATE_HIDDEN:
+            self.state_field[index_pos[1]][index_pos[0]] = STATE_OPEN
+
+    def mark(self, index_pos):
+        x, y = index_pos
+        state = self.state_field[y][x]
+        if state == STATE_HIDDEN:
+            self.state_field[y][x] = STATE_FLAGGED
+        elif state == STATE_FLAGGED:
+            self.state_field[y][x] = STATE_QUESTION
+        elif state == STATE_QUESTION:
+            self.state_field[y][x] = STATE_HIDDEN
+
+

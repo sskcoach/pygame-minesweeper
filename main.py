1ad31691f965af108aba0a66b5667545c31362df
ssk <developer.kss@gmail.com>
2025-07-30 18:02:45 +0900

    draw game over

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
     board = Board(9, 9)
 
     running = True
+    game_is_over = False
 
     while running:
 
@@ -23,15 +24,14 @@ def main():
             if event.type == pygame.MOUSEBUTTONUP:
                 game_is_over = board.on_click(event.pos, event.button)
                 if game_is_over:
-                    print("Game is Over")
                     board.open_all_mines()
-
-
-
 
         surface.fill(BLACK)
 
         board.draw(surface)
+
+        if game_is_over:
+            draw_game_over(surface)
 
         pygame.display.flip()
 
@@ -39,6 +39,13 @@ def main():
 
     pygame.quit()
     sys.exit()
+
+
+def draw_game_over(surface):
+    font = pygame.font.Font(None, 46)
+    text = font.render("Game Over", True, RED, WHITE)
+    text_rect = text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+    surface.blit(text, text_rect)
 
 
 if __name__ == "__main__":
